d7c5904ed8ad7f65fa4471855ae4a70c65d4358d
Nitendra <nitendramishra788@gmail.com>
2026-05-19 00:37:59 +0530

    Complete project CRUD APIs with JWT authentication

--- Backend/src/controllers/projectController.js
+++ Backend/src/controllers/projectController.js
@@ -31,6 +31,116 @@ const CreateProject = asyncHandler(
         });
     });
 
+    // get all projects
+
+    const getProjects = asyncHandler(
+        async(req , res)=>{
+            const Projects = await Project.find();
+
+            res.status(200).json({
+                success: true,
+                count: Projects.length,
+                Projects,
+            });
+        }
+    );
+
+
+    // get single project using by id
+
+    const getSingleProject = asyncHandler(
+        async(req , res)=>{
+            const project = await Project.findById(req.params.id);
+
+            // check if project exists or not
+
+            if(!project){
+                return res.status(404).json({
+                    success: false,
+                    message: "Project not found",
+                })
+            }
+
+            res.status(200).json({
+                success: true,
+                project,
+            })
+        }
+
+
+    )
+
+    // update project by id
+
+    const updatedProject = asyncHandler(
+        async(req , res)=>{
+            const project = await Project.findById(
+                req.params.id
+            )
+
+            // check if project exists or not
+
+            if(!project){
+                return res.status(404).json({
+                    success: false,
+                    message: "Project not found",
+                })
+            }
+
+                // update project
+
+               project.image = req.body.image || project.image;
+               project.title = req.body.title || project.title;
+               project.description = req.body.description || project.description;
+               project.live = req.body.live || project.live;
+               project.code = req.body.code || project.code;
+
+            //    save project
+            await project.save();
+
+            res.status(200).json({
+                success: true,
+                message: "Project updated successfully",
+                project: updatedProject,
+            });
+        }
+    );
+
+
+    // delete project by id
+
+    const deletedProject = asyncHandler(
+        async(req , res)=>{
+            const project= await Project.findById(
+                req.params.id
+            )
+
+            // check if project exists or not
+
+            if(!project){
+                return res.status(404).json({
+                    success: false,
+                    message: "Project not found",
+                })
+            }
+
+            // delete project
+            await project.deleteOne();
+
+            res.status(200).json({
+                success: true,
+                message: "Project deleted successfully",
+
+            })
+        }
+    );
+
+
+
     module.exports = {
     CreateProject,
+    getProjects,
+    getSingleProject,
+    updatedProject,
+    deletedProject,
     }
--- Backend/src/routes/projectRoutes.js
+++ Backend/src/routes/projectRoutes.js
@@ -2,16 +2,47 @@ const express = require("express");
 
 const {
     CreateProject,
+    getProjects,
+    getSingleProject,
+    updatedProject,
+    deletedProject,
 } = require("../controllers/projectController");
 
 const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
+// this is all projetc routes
+router.get(
+    "/",
+    getProjects
+);
+
+// get single project by id
+router.get(
+    "/:id",
+    getSingleProject
+);
+
+// create project route
 router.post(
     "/",
     authMiddleware,
     CreateProject
 );
 
+// update project route
+router.put(
+    "/:id",
+    authMiddleware,
+    updatedProject
+);
+
+// delete project route
+router.delete(
+    "/:id",
+    authMiddleware,
+    deletedProject
+
+);
 module.exports = router;
